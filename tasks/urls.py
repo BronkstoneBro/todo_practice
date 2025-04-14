8abfabc5f4813a6b0131a605d19615efa867a5c5
Бронислав <atlantwindow@gmail.com>
2025-04-14 14:23:23 +0300

    Define URL patterns for task and tag views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from .views import (
     HomePageView,
     TagListView,
@@ -9,23 +8,55 @@ from .views import (
     TagCreateView,
     TagUpdateView,
     TagDeleteView,
-    ToggleTaskStatusView
+    ToggleTaskStatusView,
 )
-
 
 app_name = "todo_list"
 
 urlpatterns = [
-    # Task urls
-    path('', HomePageView.as_view(), name='home'),
-    path('tasks/create/', TaskCreateView.as_view(), name='task_create'),
-    path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
-    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
-    path('tasks/<int:pk>/toggle/', ToggleTaskStatusView.as_view(), name='task_toggle'),
-
-    # Tag urls
-    path('tags/', TagListView.as_view(), name='tag_list'),
-    path('tags/create/', TagCreateView.as_view(), name='tag_create'),
-    path('tags/<int:pk>/update/', TagUpdateView.as_view(), name='tag_update'),
-    path('tags/<int:pk>/delete/', TagDeleteView.as_view(), name='tag_delete'),
+    path(
+        "",
+        HomePageView.as_view(),
+        name="home"
+    ),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task_create"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task_update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task_delete"
+    ),
+    path(
+        "tasks/<int:pk>/toggle/",
+        ToggleTaskStatusView.as_view(),
+        name="task_toggle"
+    ),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag_list"
+    ),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag_create"
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag_update"
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag_delete"
+    ),
 ]
